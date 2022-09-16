c5b5bfbd980f0190d9e399070ea944f42a9bffef
Majdy Jamal <majdyjamal@gmail.com>
2022-09-16 10:23:39 +0930

    Updated the auth middleware function to work with the GraphQL API.

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,13 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
+const secret = 'UpdatedNewSecretsshhhhh__:)';
 const expiration = '2h';
+
+// Updated the auth middleware function to work with the GraphQL API.
+// https://www.apollographql.com/docs/apollo-server/security/authentication/
+//
+// Middleware-specific context fields ( Express )
+//	{ req: express.Request, res: express.Response }
+// https://www.apollographql.com/docs/apollo-server/api/apollo-server/#middleware-specific-context-fields
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
+  authMiddleware: function (req) {
+    // allows token to be sent via req.query or headers
     let token = req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
@@ -16,7 +23,7 @@ module.exports = {
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -24,12 +31,10 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      console.log('Invalid token.');
     }
-
-    // send to next endpoint
-    next();
+   
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
